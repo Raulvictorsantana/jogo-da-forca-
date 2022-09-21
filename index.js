754cdbac4484d932a4531397393f0e413fe3268c
raul victor <raulvictormello@hotmail.com>
2022-09-20 21:23:09 -0300

    continuação

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
  let palavraCategoria;
+ var btn = document.getElementById("btnReiniciar");
  let palavraSorteada;
+ let lista= [];
 let  palavras = [
     
    palavra1={
@@ -36,16 +38,52 @@ function criarPalavra (){
   palavraSorteada=palavras[pegandoPalavra].nome
   palavraCategoria=palavras[pegandoPalavra].categoria
   console.log( palavraSorteada)
-  console.log( palavraCategoria)
-   //document.querySelector('.letras').innerHTML+= palavraSorteada;
-   document.getElementById('categoria').innerHTML+=palavraCategoria;}
+  console.log(palavraCategoria)
+  
+  document.querySelector('.letras').innerHTML+= palavraSorteada;
+  document.getElementById('categoria').innerHTML+=palavraCategoria;
+}
 criarPalavra ()
 
+  function mostrarNatela(){
+   const palavratelaSorteada= document.getElementById('palavra-secreta');
+   palavratelaSorteada.innerHTML ='';
 
+   const palavratelaCategoria= document.getElementById('categoria');
+   palavratelaCategoria.innerHTML = palavraCategoria;
+
+
+
+   for (let i=0; i < palavraSorteada.length; i++) {
+      if(lista[i]== undefined){
+         lista[i]= "&nbsp"
+         palavratelaSorteada.innerHTML=palavratelaSorteada.innerHTML+ "<div class='letras'>"+ lista[i]+"</div>"
+      }
+      else{
+         palavratelaSorteada.innerHTML=palavratelaSorteada.innerHTML+ "<div class='letras'>"+ lista[i]+"</div>"
+     
+      }
+   }
+}
+mostrarNatela()
+
+function verificaLetraEscolhida(letra){
+   mudarTecla("tecla-"+letra)
+   
+}
+
+function mudarTecla(tecla){
+   document.getElementById(tecla).style.background="#c71585";
+   document.getElementById(tecla).style.color="white"
+
+}
+ 
+ function atualizar(){
+   location.reload();
+
+ }
+
+btn.addEventListener("click", atualizar)
     
- //function verificaLetraEscolhida(letra){
-    let letra= document.getElementById('letra').innerHTML;
-     document.querySelector('letra').innerHTML+=letra+num
-      
- //}
+
 
