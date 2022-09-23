37c3c4500b22914aa612f3a024d425cfb6036e33
raul victor <raulvictormello@hotmail.com>
2022-09-22 21:26:01 -0300

    atualização

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
  let palavraCategoria;
- var btn = document.getElementById("btnReiniciar");
+let btn = document.getElementById("btnReiniciar");
  let palavraSorteada;
+ let tentativa =6;
  let lista= [];
 let  palavras = [
     
@@ -27,7 +28,80 @@ let  palavras = [
    palavra6={
     nome:"canada",
     categoria:"pais"
-   }
+   }, 
+   palavra8={
+      nome:"celular",
+      categoria:"objeto"
+   },
+   palavra9={
+      nome:"caneta",
+      categoria:"objeto"
+   },
+   palavra10={
+      nome:"computador",
+      categoria:"objeto"
+   },
+   palavra11={
+      nome:"copo",
+      categoria:"objeto"
+   },
+   palavra12={
+      nome:"amarelo",
+      categoria:"cor"
+   },
+   palavra13={
+      nome:"azul",
+      categoria:"cor"
+   },
+   palavra13={
+      nome:"roxo",
+      categoria:"cor"
+   },
+   palavra14={
+      nome:"preto",
+      categoria:"cor"
+   },
+   palavra15={
+      nome:"rosa",
+      categoria:"cor"
+   },
+   palavra16={
+      nome:"cachorro",
+      categoria:"animal"
+   },
+   palavra17={
+      nome:"gato",
+      categoria:"animal"
+   },
+   palavra18={
+      nome:"leao",
+      categoria:"animal"
+   },
+   palavra19={
+      nome:"peixe",
+      categoria:"animal"
+   },
+   palavra20={
+      nome:"golfinho",
+      categoria:"animal"
+   },
+   palavra21={
+      nome:"polo",
+      categoria:"carros"
+   },
+   palavra22={
+      nome:"i30",
+      categoria:"carros"
+   },
+   palavra23={
+      nome:"tcros",
+      categoria:"carros"
+   },
+   palavra7={
+      nome:"tiguan",
+      categoria:"carros"
+   },
+
     
 ]
 
@@ -61,27 +135,44 @@ criarPalavra ()
       }
       else{
          palavratelaSorteada.innerHTML=palavratelaSorteada.innerHTML+ "<div class='letras'>"+ lista[i]+"</div>"
-     
+         palavratelaSorteada.
       }
+      compararLista
    }
 }
 mostrarNatela()
 
 function verificaLetraEscolhida(letra){
    mudarTecla("tecla-"+letra)
+   compararLista(letra)
    
 }
 
 function mudarTecla(tecla){
    document.getElementById(tecla).style.background="#c71585";
-   document.getElementById(tecla).style.color="white"
+   document.getElementById(tecla).style.color="white";
 
+}
+
+function compararLista(letra){
+  const posi = palavratelaSorteada.indexOF(letra)
+  if(pos<0){
+   tentativa--
+  }
+  else {
+   for( i=0; i<palavratelaSorteada.length; i++){
+      if(palavratelaSorteada[i]=letra){
+         lista[i]=letra;
+
+      }
+   }
+}
+   
 }
  
  function atualizar(){
    location.reload();
-
- }
+}
 
 btn.addEventListener("click", atualizar)
     
